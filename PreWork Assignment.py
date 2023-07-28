a9d749738d90de6ba650f920a988447b46673d84
slidebrass <slidebrass@gmail.com>
2023-07-28 09:37:06 -0500

    first commit

--- PreWork Assignment.py
+++ PreWork Assignment.py
@@ -33,13 +33,21 @@ max_num_in_list(num_list)
     year is divisible by 4, but not divisble by 100, unless it is also
     divisible by 400. The return should be boolean Type (true/false)."""
 def is_leap_year(a_year):
-    if a_year % 4 == 0
+    if a_year % 4 == 0 and a_year % 100 != 0 or a_year % 400 == 0
 leap_message = input("Please enter a year, and I will tell you if it " +
                          "is a leap year. ")
+
+is_leap_year(leap_message)
 
 #Question 5
 """Write a function to check to see if all numbers in a list are consecutive
     numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5]
     are not consecutive numbers. The reutrn should be boolean Type."""
 
-#def is_consecutive(a_list):
+def is_consecutive(a_list): 
+    a_list.sort()
+    for item in a_list:
+        if item
+
+
+length = len(a_list)
