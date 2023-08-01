055683533caa0a6c2ba41f916b0fd6db3ee0cc00
slidebrass <55566905+slidebrass@users.noreply.github.com>
2023-07-31 20:21:04 -0500

    Delete Question 2.py

--- Question 2.py
+++
@@ -1,8 +0,0 @@
-#Question 2
-"""Write a python function, first_odds that prints the odd numbers 
-    from 1-100 and returns nothing."""
-def first_odds(odd_range):
-    for value in odd_range:
-        if value % 2 == 1:
-            print (value)
-first_odds(range(1, 101))
